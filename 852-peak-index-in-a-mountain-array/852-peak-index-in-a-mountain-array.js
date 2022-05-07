84ecdf2d93dbec4f40cdf3c241db4cf25e55d88a
Ahmed M. Hamed <49079172+AhmedHamedd2017@users.noreply.github.com>
2022-05-07 19:52:28 +0200

    Time: 75 ms (57.50%), Space: 42.2 MB (90.60%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -2,12 +2,17 @@
  * @param {number[]} arr
  * @return {number}
  */
-var peakIndexInMountainArray = function(arr) {
-    let max = -1;
-    let maxIndex = -1;
-    arr.forEach((num,index) => {
-        if(num > max) {max = num; maxIndex = index;}
-    })
+var peakIndexInMountainArray = function(arr, lo, hi) {
+    const loIndex = lo || 0;
+    const hiIndex = hi || arr.length - 1;
     
-    return maxIndex;
+    const mid = loIndex + Math.floor((hiIndex - loIndex) / 2);
+    
+    if(arr[mid] < arr[mid + 1]) return peakIndexInMountainArray(arr, mid + 1, hi);
+    
+    if(arr[mid] < arr[mid - 1]) return peakIndexInMountainArray(arr, lo, mid - 1);
+    
+    return mid
 };
+
+
